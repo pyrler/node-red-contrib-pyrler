d250249a77cca80b0a53b4e6f669728710c9253b
Kevin <kevin@roesslers.ch>
2020-02-07 22:35:34 +0100

    First release of Unifi-node

---
+++ development/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+node node_modules/node-red/red --userDir hbvisu-data
--- nodes/unifi.js
+++ nodes/unifi.js
@@ -5,31 +5,34 @@ module.exports = function(RED) {
 
     const node = this;
     node.config = config;
-    let mac;
+    let mac = "";
 
     node.on("input", (msg) => {
       if (msg.mac != undefined) {
         mac = msg.mac;
       }
+      if (mac != undefined && msg.payload != undefined) {
+        var matchingEntries = msg.payload[0].filter(function(element) {
+          return element.user === mac;
+        });
+        if (matchingEntries.length === 0) {
+        // no matching entires - don't return anything
+          return;
+        }
+
+        var element = matchingEntries[0];
+
+        if (element.key == "EVT_WU_Disconnected") {
+          node.send({payload: false});
+          node.status({fill: 'red', shape: 'dot', text: "Offline"});
+        } else if (element.key == "EVT_WU_Connected")  {
+          node.send({payload: true});
+          node.status({fill: 'green', shape: 'dot', text: "Online"});
+        }
+      }
 
       // First, search the array for any entries matching the required mac address
-      var matchingEntries = msg.payload[0].filter(function(element) {
-        return element.user === mac;
-      });
-      if (matchingEntries.length === 0) {
-      // no matching entires - don't return anything
-        return;
-      }
 
-      var element = matchingEntries[0];
-
-      if (element.key == "EVT_WU_Disconnected") {
-        node.send({payload: false});
-        node.status({fill: 'red', shape: 'dot', text: "Offline"});
-      } else if (element.key == "EVT_WU_Connected")  {
-        node.send({payload: true});
-        node.status({fill: 'green', shape: 'dot', text: "Online"});
-      }
     });
   };
   RED.nodes.registerType("unifi", add);
